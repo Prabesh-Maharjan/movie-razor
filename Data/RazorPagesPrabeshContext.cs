77c52649fb59529d4922f583151fce04e4bcd8dc
Prabesh <prabesh@inficare.com.np>
2024-03-28 00:34:34 +0200

    First commit for Razor page of Movies

---
+++ Data/RazorPagesPrabeshContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesPrabesh.Models;
+
+namespace RazorPagesPrabesh.Data
+{
+    public class RazorPagesPrabeshContext : DbContext
+    {
+        public RazorPagesPrabeshContext (DbContextOptions<RazorPagesPrabeshContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesPrabesh.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesPrabesh.Data;
+using RazorPagesPrabesh.Models;
+
+namespace RazorPagesPrabesh.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorPagesPrabesh.Data.RazorPagesPrabeshContext _context;
+
+        public DetailsModel(RazorPagesPrabesh.Data.RazorPagesPrabeshContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
